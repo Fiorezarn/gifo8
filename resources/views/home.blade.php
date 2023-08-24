d167bdbf64c76c8a9b00a80611deb7cf4502882b
fiorezarn <fioreza1902@gmail.com>
2023-08-24 10:14:23 +0700

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Home') }}</div>
+
+                <div class="card-body">
+                    <h1>Welcome to our website!</h1>
+                    <p>This is the home page content. Feel free to customize it as you like.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Gates Inspirated Fashion Outfit</title>
+        <link rel="icon" type="image/x-icon" href="assets/img/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top" id="mainNav">
+            <div class="container">
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars ml-1"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav text-uppercase ml-auto">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="/home">Login</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#profil">Profil</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#Contact">Contact</a></li>
+                       
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container">
+                <div class="masthead-heading text-uppercase">Welcome To Gates Inspirated Fashion Outfit</div>
+                <div class="masthead-subheading">Temukan Inspirasi Berpakaian Sesuai Genre Kesukaan Kalian</div>
+                <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="/home">Lets Join With Us</a>
+            </div>
+        </header>
+    
+                </div>
+            </div>
+        </section>
+        <!-- PROFIL Grid-->
+        <section class="page-section bg-light" id="profil">
+            <div class="container">
+                <div class="text-center">
+                    <h2 class="section-heading text-uppercase">Profil</h2>
+                    <h3 class="section-subheading text-muted"><br>Ahmad Miftah Fachruddin<br>10120074<br>3KA26
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="assets/img/portfolio/pnp.jpg" alt="" />
+                            </a>
+                            <div class="portfolio-caption">
+                               
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="assets/img/portfolio/pnp1.jpg" alt="" />
+                            </a>
+                            <div class="portfolio-caption">
+                              
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="assets/img/portfolio/pnp2.jpg" alt="" />
+                            </a>
+                            <div class="portfolio-caption">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4 mb-lg-0">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="assets/img/portfolio/pnp6.jpg" alt="" />
+                            </a>
+                            <div class="portfolio-caption">
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6 mb-4 mb-sm-0">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="assets/img/portfolio/pnp4.jpg" alt="" />
+                            </a>
+                            <div class="portfolio-caption">
+                               
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <div class="portfolio-item">
+                            <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
+                                <div class="portfolio-hover">
+                                    <div class="portfolio-hover-content"><i class="fas fa-plus fa-3x"></i></div>
+                                </div>
+                                <img class="img-fluid" src="assets/img/portfolio/pnp5.jpg" alt="" />
+                            </a>
+                            <div class="portfolio-caption">
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Contact-->
+        <section class="page-section" id="Contact">
+            <div class="container">
+                <div class="text-center">
+                    <h4 class="section-heading text-uppercase">Contact Us</h4>
+                    <h3 class="section-subheading text-muted"> 
+                    <br> Telp : 087772720307
+
+                    <br> Email	: amiftah81@gmail.com
+                    <br> Instagram : amf__27 <br> xxmips
+                    <br> Twitter : xxmips </h3>
+                   
+                
+                </div>
+        </section>
+        <!-- Team-->
+        
+        <!-- Clients-->
+       
+                </form>
+            </div>
+        </section>
+        <!-- Footer-->
+       
+        <!-- Portfolio Modals-->
+        <!-- Modal 1-->
+ 
+        <!-- Bootstrap core JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Third party plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <!-- Contact form JS-->
+        <script src="assets/mail/jqBootstrapValidation.js"></script>
+        <script src="assets/mail/contact_me.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
